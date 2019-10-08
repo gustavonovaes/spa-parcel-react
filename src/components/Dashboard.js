46433f6c472232b87a85a31b039e7eff4c25ef55
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-10-08 08:47:42 -0300

    Move PageWrapper to UI components folder

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
-import PageWrapper from "../components/PageWrapper";
+import PageWrapper from "./UI/PageWrapper";
 
 // https://github.com/parcel-bundler/parcel/issues/2863#issue-426218466
 const Logo = React.lazy(() => import("./Logo"));
