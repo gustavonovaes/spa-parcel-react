e21a88eb6ea4fe73cdd4d701019d1ea02f43773e
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-10-07 23:45:48 -0300

    Add charts to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 
-import Dashboard from "../components/Dashboard";
+import Dashboard from '../components/Dashboard';
+const Doughnut = React.lazy(() => import("../components/Charts/Doughnut"));
+const Line = React.lazy(() => import("../components/Charts/Line"));
 
 export default function Home() {
   return <Dashboard>
-    <h1>Home</h1>
+    <h1>Charts</h1>
+
+    <Row>
+      <Col size="1">
+        <React.Suspense fallback={<span>Loading...</span>}>
+          <Doughnut />
+        </React.Suspense>
+      </Col>
+
+      <Col size="2" style={{ flexGrow: 3 }}>
+        <React.Suspense fallback={<span>Loading...</span>}>
+          <Line />
+        </React.Suspense>
+      </Col>
+    </Row>
   </Dashboard>
 }
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const Col = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  flex: 1 0 50%;
+
+  max-width: 60vw;
+
+  margin: 40px 0;
+`
